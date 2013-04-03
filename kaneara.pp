5092e72539182d5766d178b58d103d436635b3aa
David Robbins <robbins.david@gmail.com>
2013-04-03 17:39:41 -0500

    Fixed typo

--- kaneara.pp
+++ kaneara.pp
@@ -11,7 +11,7 @@ user { "robbinsd":
   managehome => true,
 }
 
-ssh_authorized_key { "robbinsdkey"
+ssh_authorized_key { "robbinsdkey":
     ensure => present,
     key => AAAAB3NzaC1yc2EAAAADAQABAAABAQClocEEFifAjvZlkkdgvc+p0QlmjQ3NmaVFvbT0rC1AebBndmmtVexAZC+UpmaWIcHuoXqjgzGufk3l2fFPkEZAp8nvKYTa9WOik4vLALYz6g+KTjXWc4E74tRuYcOVVfg1yJqghmT7j+gzAVksgVTViMoteEWPANUo5ijC428bfLAvhEw/iaak8VVJVzmRDd/VPPCV8HEtR8no5XOK1xtXCEFpjH7oLp/gpbBXGEUqFQWMlTp7xKq0pTdzyf9WysSV4ZtlNPX9pJkHau6aTKtHRxveLHER5z5IkqQip+HWupHX+jjR4SPe/p0ChllRqEAyxYtq0TRdnqPXo6ndu/4p,
     user => "robbinsd",

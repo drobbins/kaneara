ab0415033e56a6fb7a0a9fe4cadf1a8e240f1f5d
David Robbins <robbins.david@gmail.com>
2013-04-03 18:01:43 -0500

    Added rsa type to the key

--- kaneara.pp
+++ kaneara.pp
@@ -23,4 +23,5 @@ ssh_authorized_key { "robbinsdkey":
     ensure => present,
     key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQClocEEFifAjvZlkkdgvc+p0QlmjQ3NmaVFvbT0rC1AebBndmmtVexAZC+UpmaWIcHuoXqjgzGufk3l2fFPkEZAp8nvKYTa9WOik4vLALYz6g+KTjXWc4E74tRuYcOVVfg1yJqghmT7j+gzAVksgVTViMoteEWPANUo5ijC428bfLAvhEw/iaak8VVJVzmRDd/VPPCV8HEtR8no5XOK1xtXCEFpjH7oLp/gpbBXGEUqFQWMlTp7xKq0pTdzyf9WysSV4ZtlNPX9pJkHau6aTKtHRxveLHER5z5IkqQip+HWupHX+jjR4SPe/p0ChllRqEAyxYtq0TRdnqPXo6ndu/4p",
     user => "robbinsd",
+    type => "ssh-rsa"
 }

3e340679012a5deefea709b340d91f29382f13fa
David Robbins <robbins.david@gmail.com>
2013-04-10 16:45:34 -0500

    Added www.conf for php-fpm, and the service

--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -8,11 +8,23 @@ class php {
     ensure => installed,
   }
 
+  service { 'php-fpm':
+    ensure => running,
+    require => [File['/etc/php.ini'], File['/etc/php-fpm.d/www.conf']],
+  }
+
   file { '/etc/php.ini':
     path    => '/etc/php.ini',
     ensure  => present,
     require => Package['php-fpm'],
     source  => "puppet:///modules/php/php.ini",
+  }
+
+  file { '/etc/php-fpm.d/www.conf':
+    path    => '/etc/php-fpm.d/www.conf',
+    ensure  => present,
+    require => Package['php-fpm'],
+    source  => "puppet:///modules/php/www.conf",
   }
 
 }
